505adcdc303ce420f2f6b295a592f7c2264f60e0
preeminence87 <the_nate_vasquez@hotmail.com>
2025-12-19 22:54:13 -0800

    Clarified the Discord link

--- template/icecrown/tpl/contactus.php
+++ template/icecrown/tpl/contactus.php
@@ -2,6 +2,6 @@
 ?>
 <div class="content_box1" style="line-height: 1.5;">
     <p>This is a hobby of mine, a delightful passion project. I do my best to make this a legit experience with the limited time I have to tinker, so I hope you enjoy!</p>
-    <p><a href="https://discordapp.com/users/425513561020825601" target="_blank">Discord</a></p>
+    <p><a href="https://discordapp.com/users/425513561020825601" target="_blank">Discord - Nate from IT</a></p>
     <p><?php elang('email'); ?> : nvasquez@adjutantonline.net</p>
 </div>
